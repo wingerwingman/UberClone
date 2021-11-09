6a2652c7057708f3f107c86a426d6b99ed927e92
Justin Winger <justinwinger4@gmail.com>
2021-11-09 12:53:06 -0700

    Fixed undefined in dropoff

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -2,15 +2,23 @@ import React, { useEffect, useState } from 'react'
 import tw from "tailwind-styled-components";
 import { carList } from '../data/carList.js';
 
-const RideSelector = ({pickupCoordinates, dropoffCoordinates}) => {
+const RideSelector = (props, pickupCoordinates, dropoffCoordinates) => {
 
     const [rideDuration, setRideDuration] = useState(0);
 
-    useEffect(() => {
-        rideDuration = fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoordinates[0]},${pickupCoordinates[1]};${dropoffCoordinates[0]},${dropoffCoordinates[1]}?access_token=pk.eyJ1Ijoid2luZ2Vyd2luZ21hbiIsImEiOiJja3ZtbXdrczIxbWUzMm9tdDUyaDk3azZpIn0.v7O1le6LExaRLFDizRv_6w`).then(res => res.json()).then(data => {
+    const getDirections = (pickupCoordinates, dropoffCoordinates) => {
+        fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoordinates[0]},${pickupCoordinates[1]};${dropoffCoordinates[0]},${dropoffCoordinates[1]}?access_token=pk.eyJ1Ijoid2luZ2Vyd2luZ21hbiIsImEiOiJja3ZtbXdrczIxbWUzMm9tdDUyaDk3azZpIn0.v7O1le6LExaRLFDizRv_6w`).then(res => res.json()).then(data => {
             setRideDuration(data.routes[0].duration / 100);
-        })
+            })
+    }
+    useEffect(() => {
+        if(props.pickUpCoordinates && props.dropoffCoordinates){
+            getDirections(props.pickUpCoordinates, props.dropoffCoordinates)
+        }
+        
     }, [pickupCoordinates, dropoffCoordinates])
+
+
     return (
         <Wrapper>
             <Title>Choose a Ride, or swipe up for more</Title>
@@ -23,7 +31,9 @@ const RideSelector = ({pickupCoordinates, dropoffCoordinates}) => {
                             <Service>{car.service}</Service>
                             <Time>5 min away</Time>
                         </CarDetails>
-                        <Price>{'$' + (rideDuration * car.multiplier).toFixed(2)}</Price>
+                        <Price>
+                            {'$' + (rideDuration * car.multiplier).toFixed(2)}
+                        </Price>
                     </Car>
                     )
                 })}
