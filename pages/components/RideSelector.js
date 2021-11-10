ea3d437a044d5e877a2ce816d29f92778ea14970
Justin Winger <justinwinger4@gmail.com>
2021-11-10 08:04:22 -0700

    Code updates

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -2,20 +2,20 @@ import React, { useEffect, useState } from 'react'
 import tw from "tailwind-styled-components";
 import { carList } from '../data/carList.js';
 
-const RideSelector = (props, pickupCoordinates, dropoffCoordinates) => {
+const RideSelector = (props,{ pickupCoordinates, dropoffCoordinates}) => {
+
 
     const [rideDuration, setRideDuration] = useState(0);
 
     const getDirections = (pickupCoordinates, dropoffCoordinates) => {
         fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoordinates[0]},${pickupCoordinates[1]};${dropoffCoordinates[0]},${dropoffCoordinates[1]}?access_token=pk.eyJ1Ijoid2luZ2Vyd2luZ21hbiIsImEiOiJja3ZtbXdrczIxbWUzMm9tdDUyaDk3azZpIn0.v7O1le6LExaRLFDizRv_6w`).then(res => res.json()).then(data => {
             setRideDuration(data.routes[0].duration / 100);
-            })
+        })
     }
     useEffect(() => {
-        if(props.pickUpCoordinates && props.dropoffCoordinates){
-            getDirections(props.pickUpCoordinates, props.dropoffCoordinates)
+        if(props.pickupCoordinates && props.dropoffCoordinates){
+            getDirections(props.pickupCoordinates, props.dropoffCoordinates)
         }
-        
     }, [pickupCoordinates, dropoffCoordinates])
 
 
