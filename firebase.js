19aeb520cb54ea00d27fa2ba7253f0987d4c8eef
Justin Winger <justinwinger4@gmail.com>
2021-11-10 08:27:21 -0700

    firebase env key setup

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ import { GoogleAuthProvider, getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: APIKEY,
+  apiKey: ENV['APIKEY'],
   authDomain: "uber-clone-f2cca.firebaseapp.com",
   projectId: "uber-clone-f2cca",
   storageBucket: "uber-clone-f2cca.appspot.com",
